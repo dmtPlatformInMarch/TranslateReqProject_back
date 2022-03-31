57d0ceae0b2b0e90dfc17a18f948908914db2bb6
Melona-BS <han9119693@naver.com>
2022-03-31 14:38:47 +0900

    Change whiteList

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ db.sequelize.sync({ force: false })
     });
 passportConfig();
 
-const whitelist = ["https://dmtlabs.kr/", "https://www.dmtlabs.kr/"]
+const whitelist = ["https://dmtlabs.kr", "https://www.dmtlabs.kr"]
 
 // 개발/배포 미들웨어 제어
 if (prod) {
