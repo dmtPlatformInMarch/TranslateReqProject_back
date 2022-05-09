03a1bb6dfb61ee3fd7ad539f2a56d65fc8b0c4bf
Melona-BS <han9119693@naver.com>
2022-05-09 16:52:29 +0900

    Update : DB dev

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": "DMTdatabase",
-    "host": process.env.DB_HOST,
+    "username": "root",
+    "password": "gksqjatn78",
+    "database": "DMT-database",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
