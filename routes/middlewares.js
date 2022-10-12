c4b42811a19a5cd9413719a06485c06ddde441c8
Melona-BS <han9119693@naver.com>
2022-10-12 14:04:53 +0900

    error code 410 : 로그인 필요

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -5,7 +5,7 @@ exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
-    return res.status(401).send("로그인이 필요합니다.");
+    return res.status(410).send("로그인이 필요합니다.");
 };
 
 exports.isNotLoggedIn = (req, res, next) => {
